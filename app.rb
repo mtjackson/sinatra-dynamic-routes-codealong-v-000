47f26ee43700d21c3840aa295e129c68551eee2e
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-15 21:05:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @num1 = params[:num1]
+    @solution = params[:num1]
     @num2 = params[:num2]
     "<%= @num1 * @num2 %>"
   end
